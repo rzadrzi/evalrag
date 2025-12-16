fd61dbce250ae2efb0b97c20ec1ee9fe8d33c5ff
Reza Darzi <darzi.reza@gmail.com>
2025-12-16 22:50:41 +0330

    feat: Add embeding model and vectorDB

---
+++ evalrag/core/ingestion/_vector_store.py
@@ -0,0 +1,19 @@
+from typing import List
+from langchain_core.documents import Document
+
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+
+def vector_store(embeddings, splits:List[Document]):
+    embedding_dim = len(embeddings.embed_query("hello world"))
+    index = faiss.IndexFlatL2(embedding_dim)
+
+    store = FAISS(
+        embedding_function=embeddings,
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={},
+    )
+    store.add_documents(splits)
+    store.save_local("../faiss_indexing")
