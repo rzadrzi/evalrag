0ab5df17285452b3d429bd8c7ac0787670250ff4
Reza Darzi <darzi.reza@gmail.com>
2025-12-20 18:40:54 +0330

    feat: Add a function that return embedding model based on provider that user input it

--- evalrag/core/ingestion/embedding.py
+++ evalrag/core/ingestion/embedding.py
@@ -1,3 +1,13 @@
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+def get_embedding_model(provider_name: str = "HF"):
+    embeddings = None
+    if provider_name == "HF":
+        embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-mpnet-base-v2"
+        )
+    elif provider_name == "OPENAI":
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    return embeddings
