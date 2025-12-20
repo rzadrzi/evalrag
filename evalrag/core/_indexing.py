0c4513aa62d98a36827f638800cd0fe6aec252cd
Reza Darzi <darzi.reza@gmail.com>
2025-12-20 19:44:49 +0330

    feat: Add all ingestion functions together

--- evalrag/core/_indexing.py
+++ evalrag/core/_indexing.py
@@ -1,8 +1,26 @@
 # _indexing.py
-from config import config
 
-from ingestion import pdf_loader
+from config import load_config
+from ingestion import pdf_loader, text_splitter, get_embedding_model, vector_store_
 
-path = config.ingestion.data_dir
 
-pdf_loader(path)
+def indexing():
+    setting = load_config().ingestion
+
+    docs = pdf_loader(path=setting.data_dir)
+    chuncks = text_splitter(
+        docs=docs,
+        chunk_size=setting.default_chunk_size,
+        chunk_overlap=setting.default_chunk_overlap,
+    )
+    embedding_model = get_embedding_model(provider_name=setting.provider_name)
+
+    vector_store_(
+        embeddings=embedding_model,
+        splits=chuncks,
+        vector_store_path=setting.vector_store_path,
+    )
+
+
+if __name__ == "__main__":
+    indexing()
