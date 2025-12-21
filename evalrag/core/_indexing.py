1acff21219da601a7fdc43d2bebed8d7f104efad
Reza Darzi <darzi.reza@gmail.com>
2025-12-21 05:13:42 +0330

    feat: Add all of ingestion in Indexing class only-run

--- evalrag/core/_indexing.py
+++ evalrag/core/_indexing.py
@@ -1,26 +1,5 @@
-# _indexing.py
-
-from config import load_config
-from ingestion import pdf_loader, text_splitter, get_embedding_model, vector_store_
-
-
-def indexing():
-    setting = load_config().ingestion
-
-    docs = pdf_loader(path=setting.data_dir)
-    chuncks = text_splitter(
-        docs=docs,
-        chunk_size=setting.default_chunk_size,
-        chunk_overlap=setting.default_chunk_overlap,
-    )
-    embedding_model = get_embedding_model(provider_name=setting.provider_name)
-
-    vector_store_(
-        embeddings=embedding_model,
-        splits=chuncks,
-        vector_store_path=setting.vector_store_path,
-    )
-
+# indexing.py
+from ingestion import Indexing
 
 if __name__ == "__main__":
-    indexing()
+    indexing = Indexing()
--- evalrag/core/ingestion/__init__.py
+++ evalrag/core/ingestion/__init__.py
@@ -1,7 +1,6 @@
 # __init__.py
-from .loader import pdf_loader
-from .chunkers import text_splitter
-from .embedding import get_embedding_model
-from .vector_store import vector_store_
 
-__all__ = ["pdf_loader", "text_splitter", "get_embedding_model", "vector_store_"]
+from ._indexing import Indexing
+
+
+__all__ = ["Indexing"]
