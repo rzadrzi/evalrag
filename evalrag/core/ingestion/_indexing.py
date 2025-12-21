28a4945fc54ad284e776dc12cb87bb3f6c7f782e
Reza Darzi <darzi.reza@gmail.com>
2025-12-21 08:12:48 +0330

    refact: Change strategy of ingestion

--- evalrag/core/ingestion/_indexing.py
+++ evalrag/core/ingestion/_indexing.py
@@ -1,19 +1,39 @@
 # indexing.py
-from ingestion.loader import pdf_loader
+from typing import List
+
+from langchain_core.documents import Document
+from ingestion.loader import pdf_loader, load_all_pdf
 from ingestion.chunkers import text_splitter
 from ingestion.embedding import get_embedding_model
 from ingestion.vector_store import vector_store_
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from core.config import load_config
 
 
 class Indexing:
     def __init__(self) -> None:
-        setting = load_config().ingestion
+        self.config = load_config().ingestion
 
-        docs = pdf_loader(path=setting.data_dir)
+        def loader(self) -> List[List[Document]]:
+            return load_all_pdf(path=self.config.data_dir)
 
-        splits = text_splitter(
+        def splitter(self):
+            docs = []
+
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=self.config.default_chunk_size,
+                chunk_overlap=self.config.default_chunk_overlap,
+                add_start_index=True,
+            )
+
+            for doc in self.loader():
+                docs.append(text_splitter.split_documents(doc))
+
+            return docs
+
+        """
+        splitsits = text_splitter(
             docs=docs,
             chunk_size=setting.default_chunk_size,
             chunk_overlap=setting.default_chunk_overlap,
@@ -26,3 +46,4 @@ class Indexing:
             splits=splits,
             vector_store_path=setting.vector_store_path,
         )
+        """
